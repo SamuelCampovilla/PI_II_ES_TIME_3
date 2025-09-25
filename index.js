9ff66e4b5e75ed1b192fb2a8f29f877e571b74a6
Samuel <samuelcampovilla1@icloud.com>
2025-09-25 17:55:26 -0300

    chore: initialize project with package.json and dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+const supabaseUrl = 'https://ncordpjdmjxjadnfeyg.supabase.co' 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jb3JkcGpkbWp4anhhZG5mZXlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MzcwMjAsImV4cCI6MjA3NDMxMzAyMH0.krfcElHajJjdXBHplAPACaHnrSz3RMlVydw_Pa9rrsY'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+console.log('Conexão com supabase bem sucedida.');
